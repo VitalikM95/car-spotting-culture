08e42616e269507abfd24977e28121dc2c777d0d
Vitalik <marychvitalik95@gmail.com>
2024-08-22 21:43:49 +0300

    new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import ChatList from './components/ChatList'
-import ChatWindow from './components/ChatWindow'
+import { useState } from 'react'
+import {
+  About,
+  Features,
+  Footer,
+  Header,
+  Hero,
+  Join,
+  Modal,
+  Opportunities,
+  Questions,
+  Reviews,
+  Ticker,
+} from './components'
 
 function App() {
+  const [modalActive, setModalActive] = useState(false)
   return (
-    <div className="container">
-      <ChatList />
-      <ChatWindow />
-    </div>
+    <>
+      <Header setActive={setModalActive} />
+      <div className='container'>
+        <Hero setActive={setModalActive} />
+        <About />
+      </div>
+      <Join />
+      <div className='container'>
+        <Opportunities />
+        <Features />
+      </div>
+      <Ticker setActive={setModalActive} />
+      <Reviews />
+      <Questions />
+      <Footer />
+      <Modal active={modalActive} setActive={setModalActive} />
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import store from './redux/store'
-import { Provider } from 'react-redux'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
